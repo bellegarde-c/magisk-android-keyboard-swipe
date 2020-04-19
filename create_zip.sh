19a09cb5fdd100d6ed17a510adfe052d6d136f69
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2020-04-19 17:51:34 +0200

    Update zip name

--- create_zip.sh
+++ create_zip.sh
@@ -2,4 +2,4 @@
 
 wget -O META-INF/com/google/android/update-binary https://raw.githubusercontent.com/topjohnwu/Magisk/master/scripts/module_installer.sh
 
-zip -r ../magisk-keyboard-android-swipe.zip *
+zip -r ../magisk-android-keyboard-swipe.zip *

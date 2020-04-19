4fdd1d8233561b03fa93412548ff98dc67378f91
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2020-04-19 17:48:33 +0200

    First release

---
+++ README.md
@@ -0,0 +1,5 @@
+# Swipe support for Android keyboard
+This is a Magisk module that installs swipe support to `/system/lib64`
+
+# Installation
+Download module from releases and install with Magisk
---
+++ create_zip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O META-INF/com/google/android/update-binary https://raw.githubusercontent.com/topjohnwu/Magisk/master/scripts/module_installer.sh
+
+zip -r ../magisk-keyboard-android-swipe.zip *
